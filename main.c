0d48fe7a07c0791b2834dfd6b0bcc1285a65dbef
nasiftahmid-ux <nasiftahmidpiash@gmail.com>
2026-07-21 23:48:49 +0600

    Connected course module to main

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "modules/course.h"
 
 int main() {
-    printf("In IUT, CGPA Matters!\n");
+    printf("In IUT, CGPA Matters!\n\n");
+
+    Course c1 = createCourse("CSE 4202", "Structured Programming II", 3.0);
+    viewCourse(c1);
+
     return 0;
 }
